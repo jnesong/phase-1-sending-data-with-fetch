0446e4ff475e52181101dd44a0ed0c6685eaa81a
Jenny Chau <jnechau@gmail.com>
2021-11-13 23:32:01 -0500

    commit tree 9f8ffc14960801a46ba9bb54996f716bebbf0260

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(name, email) {
+
+    const configUsers = {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            Accept: "application/json",
+        },
+        body:JSON.stringify(name, email)
+    };
+
+    return fetch ("http://localhost:3000/users", configUsers)
+    .then (response => response.json())
+    .then (jsonData => console.log(jsonData))
+    .catch (error => {
+        alert("Bad!");
+        console.log(error.message)
+    })
+}
